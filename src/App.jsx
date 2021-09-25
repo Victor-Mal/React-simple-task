08440aa419523b218e74e11141815ec5d36d1cb5
Victor Mal <VictorianusVelikyu@gmail.com>
2021-09-25 23:52:53 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Main from "./components/Main";
+import './App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+        return (
+      <div className="App" id="123" asd="asd">
+        <Main  />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main(props) {
+    console.log(props);
+  
+    return (
+        
+    );
+  }
+
+export default Main;
